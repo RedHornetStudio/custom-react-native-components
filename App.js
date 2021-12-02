b556d415afc8051ab7e17165c6337adcc12560c1
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2021-12-02 11:14:45 +0200

    created new component: CustomCard modified:   App.js new file:   components/CustomCard.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import CustomInput from './components/CustomInput';
+import CustomCard from './components/CustomCard';
 
 export default function App() {
   const [text, setText] = useState('');
@@ -11,6 +12,16 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <CustomCard style={styles.customCard}>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+      </CustomCard>
       <CustomInput style={styles.customInput} value={text} onChangeText={(text) => setText(text)} keyboardType="numeric" inputType="decimal" />
       <StatusBar style="auto" />
     </View>
@@ -22,6 +33,10 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 40,
     backgroundColor: '#fff',
+  },
+  customCard: {
+    marginVertical: 10,
+    padding: 20,
   },
   customInput: {
     width: '50%',
